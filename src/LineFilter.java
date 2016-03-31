405fc73320ac415e1d714db89bbbe9deae86ae82
Gani Marquez <iemarquez@up.edu.ph>
2016-03-31 17:14:58 +0800

    No more issues with line filter. Hard coded tho.

--- src/LineFilter.java
+++ src/LineFilter.java
@@ -5,15 +5,22 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class LineFilter {
 	public LineFilter() throws IOException {
 		ArrayList<String> data = new ArrayList<String>();
-		ArrayList<Integer> threshold = new ArrayList<Integer>();
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		String folder_name = "2016 01 (JANUARY)";
+		int year = 2016;
+		int month = 1;
+		String output_filename = "15.csv";
 		
 		for(int i = 1; i<=31; i++) {
 			try {
-		 		File file_path = new File(System.getProperty("user.home") + "/Desktop/DATA/2015 01 (JANUARY)/D-2015-1-"+ i +".log");
+		 		File file_path = new File(System.getProperty("user.home") + "/Desktop/DATA/"+ folder_name +"/D-"+ year +"-" + month +"-"+ i +".log");
 				BufferedReader br = new BufferedReader(new FileReader(file_path));
 				
 				String line = null, filtered_line = null;
@@ -22,36 +29,44 @@ public class LineFilter {
 					data.add(filtered_line);
 				}
 				
-				threshold.add(data.size());
+				map.put(i, data.size());
 			} catch(IOException e) {
 				continue;
 			}
 		}
 		
+		ArrayList<Integer> key = new ArrayList<Integer>(map.keySet());
+		Collections.sort(key);
+		
 		try {
-			FileWriter writer = new FileWriter("output.csv");
+			FileWriter writer = new FileWriter(output_filename);
 			BufferedWriter bufferedWriter = new BufferedWriter(writer);
 			
-			for(int i = 0; i<data.size(); i++) {
-				bufferedWriter.write("2015-01-"+ (i+1) + " " + data.get(i));
-				bufferedWriter.newLine();
-			}
+			int start = 0, end = 0;
+			String date;
 			
+			for(int j = 0; j<key.size(); j++) {
+				start = end;
+				end = map.get(key.get(j));
+				date = year + "-" + month + "-" + key.get(j).toString();
+				
+				for(int i = start; i<end; i++) {
+					bufferedWriter.write(date + " " + data.get(i));
+					bufferedWriter.newLine();
+				}
+			}
+				
 			bufferedWriter.close();
 			
 			System.out.println("DONE");
+			
 		} catch(IOException e1) {
 			e1.printStackTrace();
 		}
 		
 	}
 	
-	public static void main(String[] args) {
-		try {
-			new LineFilter();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws IOException {
+		new LineFilter();
 	}
 }
